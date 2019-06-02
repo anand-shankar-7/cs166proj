8eb693b3bde6d804e44e7e644e94ac1f40a7a277
Ali Cyrus Saeed <alisaeed@myth52.stanford.edu>
2019-06-02 00:15:18 -0700

    add SA and modify Makefile

---
+++ sa.cpp
@@ -0,0 +1,29 @@
+#include "sa.h"
+
+using std::string;
+
+SA::SA(const string& text) : text(text) {
+  suffix_array = (int *)malloc(text.length() * sizeof(int));
+  divsufsort((unsigned char *)text.c_str(), suffix_array, text.length());
+}
+
+SA::~SA() {
+  free(suffix_array);
+}
+
+size_t SA::findAllOccurrences(const string& pattern) {
+  return sa_search((unsigned char *)text.c_str(), text.length(), (unsigned char *)pattern.c_str(), 
+  	pattern.size(), suffix_array, text.length(), nullptr);
+}
+
+
+size_t SA::findIndex(const std::string& pattern) {
+  int32_t output_index;
+  sa_search((unsigned char *)text.c_str(), text.length(), (unsigned char *)pattern.c_str(), 
+  	pattern.size(), suffix_array, text.length(), &output_index);
+  return output_index;
+}
+
+std::string SA::getName() {
+  return "SA";
+}
---
+++ sa.h
@@ -0,0 +1,25 @@
+#ifndef _SA_h
+#define _SA_h
+
+#include <cstddef>
+#include <string>
+
+#include "libdivsufsort/build/include/divsufsort.h"
+
+class SA {
+
+  public:
+
+  SA(const std::string& text);
+  ~SA();
+  size_t findAllOccurrences(const std::string& pattern);
+  std::string getName();
+  size_t findIndex(const std::string& pattern);
+
+
+  private:
+  std::string text;
+  int *suffix_array;
+};
+
+#endif
